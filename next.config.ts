96aa1f374db79ee99d401e73c5070592b46ed25d
im-kavin <kavinhanner@icloud.com>
2025-01-29 15:49:15 -0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	output: 'standalone',
+	images: {
+		unoptimized: true,
+	},
+	webpack: (config) => {
+		if (process.env.NODE_ENV === 'development') {
+			config.externals.push({
+				sharp: 'commonjs sharp',
+			});
+		}
+		return config;
+	},
 };
 
 export default nextConfig;
